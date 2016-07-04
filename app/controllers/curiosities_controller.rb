318c0fff7380f8322aa661a6c2349816a2fb1c59
melisande noury <melisande.noury@outlook.com>
2016-07-04 20:59:44 +0200

    mise a jour controler

--- app/controllers/curiosities_controller.rb
+++ app/controllers/curiosities_controller.rb
@@ -2,10 +2,10 @@ class CuriositiesController < ApplicationController
   def index
     @curiosities = Curiosity.all
   end
-end
-def destroy
-  @curiosity = Curiosity.find(params[:id]) # Récupère l'id de la curiosité à supprimer
-  @curiosity.delete                        # Supprime la curiosité dans la base de données
+  def destroy
+    @curiosity = Curiosity.find(params[:id]) # Récupère l'id de la curiosité à supprimer
+    @curiosity.delete                        # Supprime la curiosité dans la base de données
 
-  redirect_to curiosity_path               # Redirige l'utilisateur vers la vue Index
+    redirect_to curiosities_path               # Redirige l'utilisateur vers la vue Index
+  end
 end
